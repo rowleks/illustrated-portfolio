9de198f8c98fc39ec264d837398e2344fc62ffbb
rowleks <momohrowland@gmail.com>
2024-09-15 22:53:18 +0100

    added responsive navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 function Navbar() {
+  const [open, setOpen] = useState(false)
+  const toggleNav = () => setOpen(!open)
+
+
   return (
     <nav className="navbar">
         <div className="navbar-logo">
             <img src="./logo-blk.png" alt="logo" />
             <span>Personal</span>
-        </div> 
-        <img className="nav-ham" src="hamburger.png" alt="hamburger" />
+        </div>
+
+        <div className="nav-menu">
+          <span className="nav-links">About Me</span>
+          <span className="nav-links">Skills</span>
+          <span className="nav-links">Project</span>
+          <span className="nav-links">Contact Me</span>
+        </div>
+
+        <div className="resume-btn">
+          <span>Resume</span>
+          <img src="download-icon.png" alt="" />
+        </div>
+
+        <img onClick={toggleNav} className="nav-ham" src="hamburger.png" alt="hamburger" />
+
+        <div className={`ham-menu ${open ? "open": ""}`}>
+          <span className="nav-links">About Me</span>
+          <span className="nav-links">Skills</span>
+          <span className="nav-links">Project</span>
+          <span className="nav-links">Contact Me</span>
+
+        </div>
       
     </nav>
   )
