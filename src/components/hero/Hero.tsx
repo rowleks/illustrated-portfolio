5f2f821c8accdcff3edc41eb9eff14e9729f135e
rowleks <momohrowland@gmail.com>
2024-09-16 07:39:29 +0100

    added responsive hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -7,17 +7,19 @@ return (
     <section className="hero">
         <img className="hero__img" src="/hero-illstr-mb.png" alt="hero-img" />
 
-        <div className="hero__info">
-            <p>Hello I'm <span className="span--bold">Rowland Momoh.</span></p>
-            <p><span className="span--bold">Frontend</span>  <span className="span--transparent">Developer </span></p>
-            <p>Based in <span className="span--bold">Nigeria</span></p>
-        </div>
+        <div className="hero-left">
+            <div className="hero__info">
+                <p>Hello I'm <span className="span--bold">Rowland Momoh.</span></p>
+                <p><span className="span--bold">Frontend</span>  <span className="span--transparent">Developer </span></p>
+                <p>Based in <span className="span--bold">Nigeria</span></p>
+            </div>
 
-        <div className="hero__desc">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi architecto nisi magni quos sit recusandae numquam provident at minima iste, commodi harum? Est consequatur repudiandae fugiat molestias consectetur eos ipsum?</p>
-        </div>
+            <div className="hero__desc">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi architecto nisi magni quos sit recusandae numquam provident at minima iste, commodi harum? Est consequatur repudiandae fugiat molestias consectetur eos ipsum?</p>
+            </div>
 
-        <Socials />
+            <Socials />
+        </div>
 
 
 
