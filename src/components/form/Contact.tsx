b05c2baa1b45cf663e73e55571c387fcb77a7a0e
rowleks <momohrowland@gmail.com>
2024-09-16 16:06:17 +0100

    added responsive contact section

--- src/components/form/Contact.tsx
+++ src/components/form/Contact.tsx
@@ -9,11 +9,14 @@ return (
             <input type="email" placeholder="Email" />
             <input type="text" placeholder="Your website (if exists)" />
             <textarea rows={10} placeholder="How can I help?*"></textarea>
-            <button>Get in Touch</button>
+
+            <div className="btns">
+                <button>Get in Touch</button>
+                <Socials />
+            </div>
         </form>
 
         <div className="contact__low">
-            <Socials />
             <h3>Let's <span className="span--transparent">talk</span> for Something special</h3>
             <p>I seek to push the limits of creativity to create high-engaging, user-friendly, and memorable interactive experiences.</p>
             <div className="contact__details">
